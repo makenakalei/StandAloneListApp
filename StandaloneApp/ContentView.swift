95937df07800a451375533311b6fb6b34590a33e
Makenakalei <robisonmakena@gmail.com>
2022-01-27 10:36:58 -0800

    Initial Commit

---
+++ StandaloneApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StandaloneApp
+//
+//  Created by Makena Robison on 1/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StandaloneApp/StandaloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandaloneAppApp.swift
+//  StandaloneApp
+//
+//  Created by Makena Robison on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct StandaloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
